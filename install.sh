0845e8906a85ff791736272087e4dacf437942a3
Arseny Zarechnev <cyanidesign@gmail.com>
2014-07-14 17:41:39 +0400

    make ln-s in install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ vim +BundleInstall +qall
 cd ~/.vim/bundle/vimproc.vim && make
 cd ~/.vim/bundle/YouCompleteMe && ./install.sh --clang-completer
 cd ~/.vim/bundle/tern_for_vim && npm install
+ln -s ~/.vim/vimrc ~/.vimrc
 echo "DONE!";
 exit 0;
