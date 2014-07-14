987bc992279ae11c1deb72d0b6be94d9c651d254
Arseny Zarechnev <cyanidesign@gmail.com>
2014-07-14 17:39:52 +0400

    add install.sh

--- bundles.vim
+++ bundles.vim
@@ -1,9 +1,9 @@
 set nocompatible
 filetype off
-set rtp+=~/.vim/bundle/vundle/
+set rtp+=~/.vim/bundle/Vundle.vim/
 call vundle#rc()
 
-Bundle "gmarik/vundle"
+Bundle "gmarik/Vundle.vim"
 
 " Syntax
 Bundle "jelera/vim-javascript-syntax"
---
+++ install.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +BundleInstall +qall
+cd ~/.vim/bundle/vimproc.vim && make
+cd ~/.vim/bundle/YouCompleteMe && ./install.sh --clang-completer
+cd ~/.vim/bundle/tern_for_vim && npm install
+echo "DONE!";
+exit 0;
